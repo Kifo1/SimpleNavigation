2bea949c5b9782f35d0f3b2195ee55ca77f876a5
Kifo <iakiv1203@gmail.com>
2025-02-18 16:20:40 +0100

    Implement injector

--- src/main/java/de/kifo/simpleNavigation/Main.java
+++ src/main/java/de/kifo/simpleNavigation/Main.java
@@ -1,8 +1,20 @@
 package de.kifo.simpleNavigation;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import lombok.AllArgsConstructor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import static com.google.inject.Guice.createInjector;
+
 public final class Main extends JavaPlugin {
+
+    private Injector injector;
+
+    @Override
+    public void onLoad() {
+        this.injector = createInjector();
+    }
 
     @Override
     public void onEnable() {
@@ -12,5 +24,17 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
 
+    }
+
+
+    @AllArgsConstructor
+    private class RegistrationModule extends AbstractModule {
+
+        private Main main;
+
+        @Override
+        protected void configure() {
+            bind(Main.class).toInstance(this.main);
+        }
     }
 }
