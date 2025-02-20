8fb71569c55be3f49ee55e8e86ce351bace63ce7
Kifo <iakiv1203@gmail.com>
2025-02-20 21:25:25 +0100

    Add bossbar component methode

--- src/main/java/de/kifo/simpleNavigation/common/navigation/BossbarNavigation.java
+++ src/main/java/de/kifo/simpleNavigation/common/navigation/BossbarNavigation.java
@@ -3,12 +3,14 @@ package de.kifo.simpleNavigation.common.navigation;
 import de.kifo.simpleNavigation.Main;
 import de.kifo.simpleNavigation.common.enums.NavigationType;
 import de.kifo.simpleNavigation.common.navigation.handle.Navigation;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import static de.kifo.simpleNavigation.Main.navigationService;
 import static java.lang.Math.abs;
+import static net.kyori.adventure.text.Component.text;
 import static org.bukkit.Bukkit.getScheduler;
 
 public class BossbarNavigation extends Navigation {
@@ -33,17 +35,41 @@ public class BossbarNavigation extends Navigation {
             targetDirectionYaw = targetDirectionYaw > 180 ? -360 + targetDirectionYaw : targetDirectionYaw; //Change target direction yaw range from 0-360 to -180-180 (similar to player yaw range)
             playerYaw -= targetDirectionYaw; // Subtract target direction yaw from player yaw to create a yaw system with target direction at 0
             float deltaYaw = abs(playerYaw > 180 ? 360 - playerYaw : playerYaw > 0 ? playerYaw : playerYaw < -180 ? -360 - playerYaw : playerYaw);
-            System.out.println(deltaYaw);
 
             boolean isTargetLeftForPlayer = (playerYaw > 0 && playerYaw < 180) || playerYaw < -180;  //Check if player yaw is to the left or right side of 0 with -180/180 on the opposite site of 0
-            if (isTargetLeftForPlayer) {
-                System.out.println("Target is to your left.");
-            } else {
-                System.out.println("Target is to your right.");
-            }
+            player.sendMessage(getBossBarComponent((int) deltaYaw, isTargetLeftForPlayer));
         }, 10L, 10L).getTaskId();
 
         setTaskId(taskId);
+    }
+
+    private Component getBossBarComponent(int deltaYaw, boolean isTargetLeft) {
+        if (deltaYaw <= 10) {
+            return text("---------> <---------");
+        }
+
+        if (deltaYaw >= 100) {
+            return isTargetLeft ? text("---------- ---------<") : text(">--------- ----------");
+        }
+
+        String componentText = "";
+        if (isTargetLeft) {
+            componentText += "---------- ";
+            for (int i = 0; i < 10; i++) {
+                int stageYaw = (deltaYaw - 10) - i * 10;
+                boolean isArrow = stageYaw < 0 && stageYaw >= -10;
+                componentText += isArrow ? "<" : "-";
+            }
+        } else {
+            for (int i = 0; i < 10; i++) {
+                int stageYaw = (deltaYaw - 10) - i * 10;
+                boolean isArrow = stageYaw < 0 && stageYaw >= -10;
+                componentText = (isArrow ? ">" : "-") + componentText;
+            }
+            componentText += " ----------";
+        }
+
+        return text(componentText);
     }
 
     @Override
