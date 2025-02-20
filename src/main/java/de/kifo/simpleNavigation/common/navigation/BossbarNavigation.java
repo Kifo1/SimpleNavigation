78fbc24a2d61c8262d38cb49f7074275ce960fee
Kifo <iakiv1203@gmail.com>
2025-02-20 18:06:37 +0100

    Add direction yaw calculation

--- src/main/java/de/kifo/simpleNavigation/common/navigation/BossbarNavigation.java
+++ src/main/java/de/kifo/simpleNavigation/common/navigation/BossbarNavigation.java
@@ -5,6 +5,10 @@ import de.kifo.simpleNavigation.common.enums.NavigationType;
 import de.kifo.simpleNavigation.common.navigation.handle.Navigation;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import static de.kifo.simpleNavigation.Main.navigationService;
+import static org.bukkit.Bukkit.getScheduler;
 
 public class BossbarNavigation extends Navigation {
 
@@ -14,11 +18,33 @@ public class BossbarNavigation extends Navigation {
 
     @Override
     public void start() {
+        int taskId = getScheduler().runTaskTimer(getMain(), () -> {
+            Player player = getPlayer();
+            Location targetLocation = getLocation();
 
+            if (isTargetReached()) {
+                navigationService.stopNavigation(player);
+            }
+
+            Vector targetDirection = targetLocation.toVector().setY(0).subtract(player.getLocation().toVector().setY(0));
+            float playerYaw = player.getLocation().getYaw();
+            float targetDirectionYaw = player.getLocation().clone().setDirection(targetDirection).getYaw();
+            targetDirectionYaw = targetDirectionYaw > 180 ? -360 + targetDirectionYaw : targetDirectionYaw; //Change target direction yaw range from 0-360 to -180-180 (similar to player yaw range)
+            playerYaw -= targetDirectionYaw; // Subtract target direction yaw from player yaw to create a caw system with target direction at 0
+
+            boolean isTargetLeftForPlayer = (playerYaw > 0 && playerYaw < 180) || playerYaw < -180;  //Check if player yaw is to the left or right side of 0 with -180/180 on the opposite site of 0
+            if (isTargetLeftForPlayer) {
+                System.out.println("Target is to your left.");
+            } else {
+                System.out.println("Target is to your right.");
+            }
+        }, 10L, 10L).getTaskId();
+
+        setTaskId(taskId);
     }
 
     @Override
     public void stop() {
-
+        getScheduler().cancelTask(getTaskId());
     }
 }
