da3c256ad7574359a3fce4a0f547b365dfb16cdd
Kifo <iakiv1203@gmail.com>
2025-02-25 17:50:13 +0100

    Update database structure

--- src/main/java/de/kifo/simpleNavigation/common/files/database/entities/NaviPlayer.java
+++ src/main/java/de/kifo/simpleNavigation/common/files/database/entities/NaviPlayer.java
@@ -1,14 +1,17 @@
 package de.kifo.simpleNavigation.common.files.database.entities;
 
+import de.kifo.simpleNavigation.common.enums.NavigationType;
 import lombok.Data;
 
 import java.util.UUID;
+
+import static de.kifo.simpleNavigation.common.enums.NavigationType.PARTICLES;
 
 @Data
 public class NaviPlayer {
 
     private final UUID uuid;
-    private PlayerSettings playerSettings;
+    private NavigationType preferredNavigationType = PARTICLES;
 
 }
 
