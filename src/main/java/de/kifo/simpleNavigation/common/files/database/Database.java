52b0e0dd72aeee03a378a9ba0a8c0ccccf50dc62
Kifo <iakiv1203@gmail.com>
2025-02-25 18:24:07 +0100

    Create database methods to load and save navi players

--- src/main/java/de/kifo/simpleNavigation/common/files/database/Database.java
+++ src/main/java/de/kifo/simpleNavigation/common/files/database/Database.java
@@ -1,9 +1,14 @@
 package de.kifo.simpleNavigation.common.files.database;
 
+import de.kifo.simpleNavigation.common.enums.NavigationType;
+import de.kifo.simpleNavigation.common.files.database.entities.NaviPlayer;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
@@ -45,6 +50,10 @@ public class Database {
         initializeNaviPlayerTable();
     }
 
+    /**
+     *  {@link NaviPlayer}
+     */
+
     private void initializeNaviPlayerTable() throws SQLException {
         Connection connection = getConnection();
 
@@ -54,5 +63,39 @@ public class Database {
 
         statement.executeUpdate();
         statement.close();
+    }
+
+    public void saveNaviPlayer(NaviPlayer naviPlayer) {
+        try {
+            PreparedStatement statement = getConnection()
+                    .prepareStatement("INSERT OR REPLACE INTO navi_player (uuid, preferred_navigation_type) VALUES (?, ?)");
+
+            statement.setString(1, naviPlayer.getUuid().toString());
+            statement.setString(2, naviPlayer.getPreferredNavigationType().toString());
+
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public NaviPlayer loadNaviPlayer(UUID uuid) {
+        try {
+            PreparedStatement statement = getConnection()
+                    .prepareStatement("SELECT * FROM navi_player WHERE uuid = ?");
+
+            statement.setString(1, uuid.toString());
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return new NaviPlayer(uuid, NavigationType.valueOf(resultSet.getString("preferred_navigation_type")));
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
--- src/main/java/de/kifo/simpleNavigation/common/files/database/entities/NaviPlayer.java
+++ src/main/java/de/kifo/simpleNavigation/common/files/database/entities/NaviPlayer.java
@@ -1,6 +1,7 @@
 package de.kifo.simpleNavigation.common.files.database.entities;
 
 import de.kifo.simpleNavigation.common.enums.NavigationType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.UUID;
@@ -8,6 +9,7 @@ import java.util.UUID;
 import static de.kifo.simpleNavigation.common.enums.NavigationType.PARTICLES;
 
 @Data
+@AllArgsConstructor
 public class NaviPlayer {
 
     private final UUID uuid;
