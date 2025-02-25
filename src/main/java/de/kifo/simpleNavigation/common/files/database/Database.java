55d89cb4d1f1f2db91f412bf0940400ddfaa46fd
Kifo <iakiv1203@gmail.com>
2025-02-25 19:23:12 +0100

    Create new navi player when uuid can't be found within the database

--- src/main/java/de/kifo/simpleNavigation/common/files/database/Database.java
+++ src/main/java/de/kifo/simpleNavigation/common/files/database/Database.java
@@ -96,6 +96,6 @@ public class Database {
             e.printStackTrace();
         }
 
-        return null;
+        return new NaviPlayer(uuid);
     }
 }
--- src/main/java/de/kifo/simpleNavigation/common/files/database/entities/NaviPlayer.java
+++ src/main/java/de/kifo/simpleNavigation/common/files/database/entities/NaviPlayer.java
@@ -3,6 +3,7 @@ package de.kifo.simpleNavigation.common.files.database.entities;
 import de.kifo.simpleNavigation.common.enums.NavigationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
@@ -10,6 +11,7 @@ import static de.kifo.simpleNavigation.common.enums.NavigationType.PARTICLES;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class NaviPlayer {
 
     private final UUID uuid;
